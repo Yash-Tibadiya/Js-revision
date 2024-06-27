dc1592f1edacd393460773fb2b9fef809903a67d
Yash-Tibadiya <tibadiyayash@gmail.com>
2024-06-27 14:55:48 +0530

    add types of memories in x01

--- x01_Datatypes-summary.js
+++ x01_Datatypes-summary.js
@@ -41,3 +41,46 @@ const myFunction = function () {
 };
 
 console.log(typeof heros);
+
+// *************************************************************************
+
+// 2 types of memories are use
+// Stack (Primitive)
+// Heap (non primitive)
+
+// Stack
+let myName = "yaash";
+
+let anotherName = myName;
+anotherName = "Yash";
+
+console.log(myName);
+console.log(anotherName);
+
+// Heap
+let user = {
+  email: "user@example.com",
+  upi: "user@oksbi",
+};
+
+let userX = user;
+userX.email = "user@gmail.com";
+
+console.log(user.email);
+console.log(userX.email);
+
+// Stack
+// Memory Allocation: Static and fixed size.
+// Allocation/Deallocation: Managed automatically by the compiler. Allocation and deallocation are very fast and occur in a Last In, First Out (LIFO) order.
+// Usage: Stores primitive data types (numbers, booleans, etc.), function call frames (including local variables and return addresses), and reference pointers to heap objects.
+// Lifetime: Variables are automatically deallocated when they go out of scope.
+// Access Speed: Generally faster due to its structured access pattern.
+// Size: Typically smaller and limited compared to the heap.
+
+// Heap
+// Memory Allocation: Dynamic and can grow as needed.
+// Allocation/Deallocation: Managed manually (explicitly in languages like C/C++, or automatically by a garbage collector in languages like JavaScript). Allocation is slower compared to the stack.
+// Usage: Stores objects, dynamic data structures (arrays, linked lists, etc.), and anything that needs to persist beyond the current function call.
+// Lifetime: Variables persist until they are explicitly deallocated or garbage collected.
+// Access Speed: Generally slower due to the complexity of memory management and lack of structured access patterns.
+// Size: Larger and can grow as needed, limited by the total available memory.
