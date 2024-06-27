b98ef8b39ca4c170c393171822d076cfa959c4df
Yash-Tibadiya <tibadiyayash@gmail.com>
2024-06-27 13:52:02 +0530

    add primitive data types in x01

---
+++ 05_Datatypes-summary.js
@@ -0,0 +1,28 @@
+// JavaScript is a dynamically typed language
+// Typescript is a statically typed language
+
+// primitive
+// 7 types : String, Number, Boolean, BigInt, Symbol, null, undefined
+
+const x = 100; // Number
+const y = 100.69; // number
+
+const isLoggedIn = true; // boolean
+const outsideTemp = null; // object
+
+let userEmail; // undefined
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+// same value symbol are different
+console.log(id == anotherId);  // false
+console.log(id === anotherId);  // false
+
+const bigNumber = 33556575423154235123151n
+console.log(typeof bigNumber);
+
+// #################################################################
+
+// Reference (non primitive)
+// Array, Object, Function
+
