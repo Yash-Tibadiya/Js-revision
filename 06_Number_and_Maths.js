04e6d71a737ea76880b9fc3a3e73a55b1896a233
Yash-Tibadiya <tibadiyayash@gmail.com>
2024-06-30 23:22:39 +0530

    add number in 06

---
+++ 06_Number_and_Maths.js
@@ -0,0 +1,16 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 3328.2338
+console.log(otherNumber.toPrecision(5));
+console.log(otherNumber.toPrecision(3));
+
+const oneM = 1000000
+console.log(oneM.toLocaleString('en-US'));
+console.log(oneM.toLocaleString('en-IN'));
