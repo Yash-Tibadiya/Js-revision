497f600aef2729976d7cacc6b8546f622fc7f422
Yash-Tibadiya <tibadiyayash@gmail.com>
2024-07-04 14:00:28 +0530

    add scopes in 15

---
+++ 15_scopes.js
@@ -0,0 +1,16 @@
+var c = 300; // global scope
+let a = 300; // global scope
+
+if (true) {
+  let a = 10;
+  const b = 20;
+  var c = 30;
+  console.log("Inner: ", a);
+}
+
+console.log("Outer: ", a); // it is block scope
+// console.log(b); // not run because it is block scope
+
+console.log(c); // chnage the c value 300 to 30
+
+// code in browser and run using node both have different scope
