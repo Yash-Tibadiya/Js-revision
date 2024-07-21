fa0585e66082666ff13c72413fad132650c3733a
Yash-Tibadiya <tibadiyayash@gmail.com>
2024-07-21 22:28:18 +0530

    add 23

---
+++ 23_While and do while.js
@@ -0,0 +1,23 @@
+// while
+
+let index = 0;
+while (index <= 10) {
+  console.log(`value of index: ${index}`);
+  index = index + 2;
+}
+
+const myArray = ["Captain America", "Iron Man", "Thor"];
+let Superhero = 0;
+
+while (Superhero < myArray.length) {
+  console.log(`value of arr: ${myArray[Superhero]}`);
+  Superhero++;
+}
+
+// do while
+let score = 1;
+// let score = 11;
+do {
+  console.log(`Score: ${score}`);
+  score++;
+} while (score <= 10);
