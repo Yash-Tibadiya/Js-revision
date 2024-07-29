ea9f15fe9d847d67e2af5cd1cc2d007b727b1623
Yash-Tibadiya <tibadiyayash@gmail.com>
2024-07-29 23:22:26 +0530

    add p1

---
+++ P1_color changer.html/script.js
@@ -0,0 +1,22 @@
+const btn = document.querySelectorAll(".button");
+const bd = document.querySelector("body");
+
+btn.forEach(function (button) {
+  console.log(button);
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === "grey") {
+      bd.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "white") {
+      bd.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "blue") {
+      bd.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      bd.style.backgroundColor = e.target.id;
+    }
+  });
+});
